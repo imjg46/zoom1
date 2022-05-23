f820d5dd1fbc617695c56186c038f6b5adec3aa6
imjg46 <yanndch2019@yandex.ru>
2022-05-23 16:14:38 +0900

    error

--- src/server.js
+++ src/server.js
@@ -5,7 +5,9 @@ const __dirname = path.resolve();
 const app = express();
 
 app.set("view engine", "pug");
-app.set("views", __dirname + "/src/views");
+app.set("views" + __dirname + "/src/views");
+
+app.get("/", (req, res) => res.render("home"));
 
 const handleListen = () => console.log("http://localhost:3000");
 app.listen(3000);
