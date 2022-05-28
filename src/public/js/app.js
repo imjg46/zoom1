fc38805503c55cd797daa5e2cc09be9490e2998b
imjg46 <67748736+imjg46@users.noreply.github.com>
2022-05-28 18:55:44 +0900

    end basic chat
    Good!!!

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,33 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
-
+const nickForm = document.querySelector("form#nick"); //#nick
+const messageForm = document.querySelector("form#message"); //#message
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload){
+    const msg = { type, payload };
+    return JSON.stringify(msg); //JSObject를 String으로 변경
+}
 
 socket.addEventListener("open", (open) => { //BackEnd와 연결될때
     console.log("✅ Connected to server!");
+    nickForm.querySelector("input").value = ""; //****왜그런진 모르겠는데 새로고침 하니까 입력창엔 뜨는데 적용은 안되서 이렇게 둠 ****/
 });
 
-socket.addEventListener("message", async (event) => { //BackEnd에서 메시지 올때
-    console.log("💌 MSG: " + await event.data.text());
-    //ws버전8되면서,    https://nomadcoders.co/noom/lectures/3091
-});
+// socket.addEventListener("message", async (event) => { //BackEnd에서 메시지 올때  // #1.8에서 안되서 바꿈
+//     //console.log("💌 MSG: " + await event.data.text()); //ws버전8되면서,    https://nomadcoders.co/noom/lectures/3091
+//     const li = document.createElement("li"); //메시지 화면에 표시
+//     //li.innerText = await event.data.text();
+//     li.innerText = "working";
+//     messageList.append(li);
+// });
 
-socket.addEventListener("close", (close) => { //BackEnd와 연결 끊길때
+socket.addEventListener("message", (message) => { //BackEnd에서 메시지 올때
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
+  });
+
+socket.addEventListener("close", () => { //BackEnd와 연결 끊길때
     console.log("❌ Disconnected from server!");
 });
 
@@ -20,11 +35,18 @@ socket.addEventListener("close", (close) => { //BackEnd와 연결 끊길때
 //     socket.send("hello from browser!"); //메시지 BackEnd로 보내기
 // }, 5000)
 
-function handleSumbit(event){
+function handleMessageSumbit(event){ //메시지 전송 (=handleSubmit)
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    socket.send(input.value);
+    socket.send(makeMessage("message", input.value));
     input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSumbit);
+function handleNickSubmit(event){ //닉네임 변경
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    socket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleMessageSumbit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -2,34 +2,45 @@ import express from "express";
 import http from "http";
 import WebSocket, {WebSocketServer}  from "ws";
 import path from "path";
+//import { type } from "express/lib/response";
 const __dirname = path.resolve();
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views"); //src지워줘야 할수도 있음
 app.use("/public", express.static(__dirname + "/src/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/ *", (req, res) => res.redirect("/")); // example.com/abc 접속시, example.com으로 이동
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/")); // example.com/abc 접속시, example.com으로 이동
 
-const handleListen = () => console.log("😀 Listening on http://localhost:3000");
+const handleListen = () => console.log(`😀 Listening on http://localhost:3000`);
 //app.listen(3000, handleListen); //express는 WebSocket지원 안함
 const server = http.createServer(app); //HTTP SERVER
 const wss = new WebSocketServer( { server } ); //WEBSOCKET SERVER + http서버와 같이 (항상 필요 x)
 
 const sockets_ = [];
 
-wss.on("connection", (socket) => { //FrontEnd와 연결될때
+wss.on("connection", (socket, req) => { //FrontEnd와 연결될때
+    const ip = req.socket.remoteAddress; //사용자ip 얻기
     sockets_.push(socket); //socket을 받을때 sockets[]에 넣기
-
-    console.log("✅ Connected to browser!");
-
-    socket.on("close", (socket) => { //FrontEnd와 연결 끊길때
+    socket["nickname"] = "Anonymous";
+    console.log("✅ Connected to browser! ("+ip+")");
+    socket.on("close", () => { //FrontEnd와 연결 끊길때
         console.log("❌ Disconnected from browser!");
     });
-
-    socket.on("message", (message) => { //FrontEnd로부터 메시지 올때
+    socket.on("message", (msg) => { //FrontEnd로부터 메시지 올때
         //socket.send(message); //메시지 FrontEnd로 보내주기
-        sockets_.forEach(aSocket => aSocket.send(message));
+        const parsed_msg = JSON.parse(msg); //JSON -> JavaScript Object
+        //console.log(parsed_msg.type+":"+parsed_msg.payload);
+        switch(parsed_msg.type){
+            case "message":
+                sockets_.forEach((aSocket) => 
+                    aSocket.send(`${socket.nickname}: ${parsed_msg.payload}`)
+                );
+                break;
+            case "nickname":
+                socket["nickname"] = parsed_msg.payload; //socket["nickname"]에 닉네임 저장
+                break;
+        }
         //console.log("💌 MSG: " + message.toString());
     });
 
