5061b4c7d4c775e24f2c48530b8e2479d0426fea
imjg46 <yanndch2019@yandex.ru>
2022-05-28 10:36:13 +0900

    upload

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,30 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", (open) => { //BackEnd와 연결될때
+    console.log("✅ Connected to server!");
+});
+
+socket.addEventListener("message", async (event) => { //BackEnd에서 메시지 올때
+    console.log("💌 MSG: " + await event.data.text());
+    //ws버전8되면서,    https://nomadcoders.co/noom/lectures/3091
+});
+
+socket.addEventListener("close", (close) => { //BackEnd와 연결 끊길때
+    console.log("❌ Disconnected from server!");
+});
+
+// setTimeout(() => {
+//     socket.send("hello from browser!"); //메시지 BackEnd로 보내기
+// }, 5000)
+
+function handleSumbit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    socket.send(input.value);
+    input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSumbit);
--- src/server.js
+++ src/server.js
@@ -11,15 +11,29 @@ app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/ *", (req, res) => res.redirect("/")); // example.com/abc 접속시, example.com으로 이동
 
-const handleListen = () => console.log("^v^ Listening on http://localhost:3000");
+const handleListen = () => console.log("😀 Listening on http://localhost:3000");
 //app.listen(3000, handleListen); //express는 WebSocket지원 안함
 const server = http.createServer(app); //HTTP SERVER
 const wss = new WebSocketServer( { server } ); //WEBSOCKET SERVER + http서버와 같이 (항상 필요 x)
 
-function handleConnection(socket) {
-    console.log(socket);
-}
+const sockets_ = [];
 
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => { //FrontEnd와 연결될때
+    sockets_.push(socket); //socket을 받을때 sockets[]에 넣기
+
+    console.log("✅ Connected to browser!");
+
+    socket.on("close", (socket) => { //FrontEnd와 연결 끊길때
+        console.log("❌ Disconnected from browser!");
+    });
+
+    socket.on("message", (message) => { //FrontEnd로부터 메시지 올때
+        //socket.send(message); //메시지 FrontEnd로 보내주기
+        sockets_.forEach(aSocket => aSocket.send(message));
+        //console.log("💌 MSG: " + message.toString());
+    });
+
+    //socket.send("hello"); //메시지 FrontEnd로 보내기
+});
 
 server.listen(3000, handleListen);
